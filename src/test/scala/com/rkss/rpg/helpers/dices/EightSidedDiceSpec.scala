25f1dce83cd5db7943148bc8d450e897e577cf82
Malkaviano <rafael.kss@gmail.com>
2021-12-20 11:25:37 -0300

    [FEATURE] Add eight sided dice

---
+++ src/test/scala/com/rkss/rpg/helpers/dices/EightSidedDiceSpec.scala
@@ -0,0 +1,26 @@
+package com.rkss.rpg.helpers.dices
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalamock.scalatest.MockFactory
+
+class EightSidedDiceSpec
+    extends AnyFunSpec
+    with Matchers
+    with MockFactory
+    with BehavesLikeDice {
+  describe("Eight Sided Dice roll") {
+    val rollD8 = mockFunction[DiceRange, Int]
+
+    val dice = EightSidedDice(rollD8)
+
+    val name = "D8"
+    val range = EightSidedDice.range
+
+    behavesLikeDice(dice, name, range, rollD8, 5)
+
+    it(s"should have range (1, 8)") {
+      EightSidedDice.range shouldBe (DiceRange(1, 8))
+    }
+  }
+}
