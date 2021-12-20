37b49b58e13ee5a0fb5635c18c1a561523e15769
Malkaviano <rafael.kss@gmail.com>
2021-12-20 12:55:25 -0300

    [FEATURE] Restrict AbstractDice and DiceRange inside package

--- src/main/scala/com/rkss/rpg/dices/AbstractDice.scala
+++ src/main/scala/com/rkss/rpg/dices/AbstractDice.scala
@@ -3,7 +3,7 @@ package com.rkss.rpg.dices
 import com.rkss.rpg.traits.Dice
 import com.rkss.rpg.traits.DiceName
 
-abstract class AbstractDice(
+abstract class AbstractDice private[dices](
     private val rng: DiceRange => Int,
     private val range: DiceRange,
     override val name: DiceName
--- src/main/scala/com/rkss/rpg/dices/DiceRange.scala
+++ src/main/scala/com/rkss/rpg/dices/DiceRange.scala
@@ -1,3 +1,3 @@
 package com.rkss.rpg.dices
 
-final case class DiceRange(val min: Int, val max: Int)
+final case class DiceRange private[dices](val min: Int, val max: Int)
